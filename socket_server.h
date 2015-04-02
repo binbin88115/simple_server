1b8c6a0172700bcf6e89024804b1b82bf2f44b37
binbin88115 <mxp@mxpdeMac-mini.local>
2015-04-02 14:30:51 +0800

    使用云风socket库

--- socket_server.h
+++ socket_server.h
@@ -1,25 +1,40 @@
-#ifndef _SOCKET_SERVER_H_
-#define _SOCKET_SERVER_H_
+#ifndef skynet_socket_server_h
+#define skynet_socket_server_h
 
-#define SOCKET_ERROR  -1
-#define SOCKET_OK      0
-#define SOCKET_ACCEPT  1
-#define SOCKET_LISTEN  2
+#include <stdint.h>
+
+#define SOCKET_DATA 0
+#define SOCKET_CLOSE 1
+#define SOCKET_OPEN 2
+#define SOCKET_ACCEPT 3
+#define SOCKET_ERROR 4
+#define SOCKET_EXIT 5
 
 struct socket_server;
-struct socket;
 
-struct socket_message
-{
-
+struct socket_message {
+	int id;
+	uintptr_t opaque;
+	int ud;	// for accept, ud is listen id ; for data, ud is size of data 
+	char * data;
 };
 
-struct socket_server* socket_server_create();
-void socket_server_release(struct socket_server* ss);
+struct socket_server * socket_server_create();
+void socket_server_release(struct socket_server *);
+int socket_server_poll(struct socket_server *, struct socket_message *result, int *more);
 
-int socket_server_start(struct socket_server* ss, const char* host, int port, int backlog);
-int socket_server_wait(struct socket_server* ss, struct socket_message* message);
+void socket_server_exit(struct socket_server *);
+void socket_server_close(struct socket_server *, uintptr_t opaque, int id);
+void socket_server_start(struct socket_server *, uintptr_t opaque, int id);
 
-int socket_server_send(struct socket_server* ss, int fd, int sz, void* buf);
+// return -1 when error
+int64_t socket_server_send(struct socket_server *, int id, const void * buffer, int sz);
 
-#endif // _SOCKET_SERVER_H_
+// ctrl command below returns id
+int socket_server_listen(struct socket_server *, uintptr_t opaque, const char * addr, int port, int backlog);
+int socket_server_connect(struct socket_server *, uintptr_t opaque, const char * addr, int port);
+int socket_server_bind(struct socket_server *, uintptr_t opaque, int fd);
+
+int socket_server_block_connect(struct socket_server *, uintptr_t opaque, const char * addr, int port);
+
+#endif
--- test.c
+++ test.c
@@ -1,43 +1,73 @@
 #include "socket_server.h"
 
+#include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
 
-int main()
-{
-    struct socket_server* ss = socket_server_create();
-    int ret = socket_server_start(ss, "127.0.0.1", 8888, 10);
-    printf("============start server: %d\n", ret);
-    struct socket_message message;
-    for (;;) {
-        int code = socket_server_wait(ss, &message);
-        switch (code) {
-        case SOCKET_LISTEN:
-            break;
-        case SOCKET_ACCEPT:
-            break;
-        default:
-            break;
-        }
-    }
-    socket_server_release(ss);
-    /*
-        struct socket_server* ss = socker_server_create();
-        socket_server_listen(ss, "localhost", 8888, 100);
-        for (;;) {
-            struct socket_message message;
-            int ret = socket_server_wait(ss, &message);
-            switch (ret) {
-                case SOCKET_DATA:
-                    message.data, message.sz
-                    break;
-                case SOCKET_ACCEPT:
-                    message.fd
-                    break;
-                case SOCKET_CLOSE:
-                    break;
-            }           
-        }
-    */
-    
-    return 0;
+static void *
+_poll(void * ud) {
+	struct socket_server *ss = ud;
+	struct socket_message result;
+	for (;;) {
+		int type = socket_server_poll(ss, &result, NULL);
+		// DO NOT use any ctrl command (socket_server_close , etc. ) in this thread.
+		switch (type) {
+		case SOCKET_EXIT:
+			return NULL;
+		case SOCKET_DATA:
+			printf("message(%lu) [id=%d] size=%d\n",result.opaque,result.id, result.ud);
+			free(result.data);
+			break;
+		case SOCKET_CLOSE:
+			printf("close(%lu) [id=%d]\n",result.opaque,result.id);
+			break;
+		case SOCKET_OPEN:
+			printf("open(%lu) [id=%d] %s\n",result.opaque,result.id,result.data);
+			break;
+		case SOCKET_ERROR:
+			printf("error(%lu) [id=%d]\n",result.opaque,result.id);
+			break;
+		case SOCKET_ACCEPT:
+			printf("accept(%lu) [id=%d %s] from [%d]\n",result.opaque, result.ud, result.data, result.id);
+			socket_server_start(ss, result.opaque, result.ud);
+			break;
+		}
+	}
+}
+
+static void
+test(struct socket_server *ss) {
+	pthread_t pid;
+	pthread_create(&pid, NULL, _poll, ss);
+
+	// int c = socket_server_connect(ss,100,"127.0.0.1",80);
+	// printf("connecting %d\n",c);
+	int l = socket_server_listen(ss,200,"127.0.0.1",8888,32);
+	printf("listening %d\n",l);
+	socket_server_start(ss,201,l);
+	// int b = socket_server_bind(ss,300,l);
+	// printf("binding stdin %d\n",b);
+	// int i;
+	// for (i=0;i<10;i++) {
+	// 	socket_server_connect(ss, 0, "127.0.0.1", 8888);
+	// }
+	sleep(1000);
+	socket_server_exit(ss);
+
+	pthread_join(pid, NULL); 
+}
+
+int
+main() {
+	struct sigaction sa;
+	sa.sa_handler = SIG_IGN;
+	sigaction(SIGPIPE, &sa, 0);
+
+	struct socket_server * ss = socket_server_create();
+	test(ss);
+	socket_server_release(ss);
+
+	return 0;
 }
